29be51d97a10d0e415f250c87ddcffde8e6db5ee
Gabe Goodhart <ghart@us.ibm.com>
2025-05-20 13:18:55 -0600

    feat: Add can_seq_rm API to llama_kv_cache API
    This will be key for the hybrid cache which needs to be able to validate
    that all children can perform seq_rm cleanly before attempting to remove
    the seq from any single child to avoid ending up in a corrupted state.
    
    Branch: HybridCache
    
    Signed-off-by: Gabe Goodhart <ghart@us.ibm.com>

--- src/llama-kv-cache.h
+++ src/llama-kv-cache.h
@@ -39,6 +39,11 @@ struct llama_kv_cache : public llama_memory_i {
     // TODO: remove
     virtual void set_full() = 0;
 
+    // sometimes it is useful to check whether a cache can remove a sequence
+    // before attempting to mutate the cache (eg a hybrid cache with multiple
+    // children to keep in sync)
+    virtual bool can_seq_rm(llama_seq_id seq_id, llama_pos p0, llama_pos p1) const = 0;
+
     //
     // batch processing
     //
@@ -141,6 +146,8 @@ public:
     void defrag_sched(float thold) override;
 
     void set_full() override;
+
+    bool can_seq_rm(llama_seq_id seq_id, llama_pos p0, llama_pos p1) const override;
 
     llama_sbatch sbatch_init(const llama_batch & batch, bool logits_all) override;
     llama_ubatch ubatch_next(llama_sbatch & sbatch, uint32_t n_ubatch, bool embd_pooled) const override;
@@ -331,6 +338,8 @@ public:
 
     void set_full() override;
 
+    bool can_seq_rm(llama_seq_id seq_id, llama_pos p0, llama_pos p1) const override;
+
     llama_sbatch sbatch_init(const llama_batch & batch, bool logits_all) override;
     llama_ubatch ubatch_next(llama_sbatch & sbatch, uint32_t n_ubatch, bool embd_pooled) const override;
 
@@ -436,6 +445,8 @@ public:
     void defrag_sched(float thold) override;
 
     void set_full() override;
+
+    bool can_seq_rm(llama_seq_id seq_id, llama_pos p0, llama_pos p1) const override;
 
     llama_sbatch sbatch_init(const llama_batch & batch, bool logits_all) override;
     llama_ubatch ubatch_next(llama_sbatch & sbatch, uint32_t n_ubatch, bool embd_pooled) const override;
